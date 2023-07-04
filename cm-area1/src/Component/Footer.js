73025469dceebd0c5a027967e69c43fa289f470b
artjitt9611 <artjitt2512@gmail.com>
2023-07-04 10:20:16 +0700

    set footer line

--- cm-area1/src/Component/Footer.js
+++ cm-area1/src/Component/Footer.js
@@ -9,7 +9,7 @@ import ScrollToTop from "react-scroll-to-top";
 import axios from "axios";
 import { Card } from "antd";
 const { REACT_APP_GA } = process.env;
-
+const { REACT_APP_PATH2 } = process.env;
 export const Footer = (className) => {
     const [Online, setOnline] = useState("");
     const [All, setAll] = useState("");
@@ -38,38 +38,34 @@ export const Footer = (className) => {
         get();
     }, []);
 
+    const [Contact, setContact] = useState();
+
+    useEffect(() => {
+        async function get() {
+            axios.get(`${REACT_APP_PATH2}/admin/api/getOther`).then((res) => {
+                setContact(res.data[0].array);
+            });
+        }
+        get();
+    }, []);
+
+    console.log(Contact);
+
     return (
         <footer className="footer">
             <ScrollToTop smooth />
             <Container>
                 <Row>
                     <Col xs={12} md={8}>
-                        <p>
-                            สำนักงานเขตพื้นที่การศึกษาประถมศึกษาเชียงใหม่ เขต 1 อาคารอำนวยการกลาง ชั้น 4
-                            ศาลากลางจังหวัดเชียงใหม่ อำเภอเมืองเชียงใหม่ จังหวัดเชียงใหม่ 50300
-                        </p>
-                        <p>
-                            <a
-                                href="https://www.facebook.com/profile.php?id=100057185991041"
-                                target="_blank"
-                                rel="noreferrer"
-                            >
-                                <FontAwesomeIcon icon={faFacebook} style={{ paddingRight: "10px" }} />
-                                Facebook
-                            </a>
-                        </p>
-                        <p>
-                            <FontAwesomeIcon icon={faPhone} style={{ paddingRight: "10px" }} />
-                            ติดต่อเราได้ที่ : 053-112333
-                        </p>
-                        <p>
-                            <FontAwesomeIcon icon={faPhone} style={{ paddingRight: "10px" }} />
-                            โทรสาร : 053-112677 ต่อ 319 : กลุ่มส่งเสริมการศึกษาทางไกล เทคโนโลยีสารสนเทศและการสื่อสาร
-                        </p>
-                        <p>
-                            <FontAwesomeIcon icon={faEnvelope} style={{ paddingRight: "10px" }} />
-                            ict@chiangmaiarea1.go.th
-                        </p>
+                        {Contact &&
+                            Contact.map((item, index) => (
+                                <p
+                                    key={index}
+                                    style={{ whiteSpace: "pre-wrap", overflow: "hidden", textOverflow: "ellipsis" }}
+                                >
+                                    {item}
+                                </p>
+                            ))}
                     </Col>
 
                     <Col xs={6} md={4} className="text-center text-sm-end">
